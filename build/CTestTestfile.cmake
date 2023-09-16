85d5ba6ef31a5a290ab586454f554cdcd9b2ba8d
Ashwath Karthikeyan <ashwath.karthikeyan@gmail.com>
2023-09-15 19:38:24 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashwath/quadruped_robot/src
+# Build directory: /home/ashwath/quadruped_robot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("controller")
+subdirs("joypad")
+subdirs("robot_description")
+subdirs("spot")
+subdirs("spot_gazebo")
---
+++ build/spot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashwath/quadruped_robot/src/spot
+# Build directory: /home/ashwath/quadruped_robot/build/spot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
